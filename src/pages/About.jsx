194fc61db5ad3083e3eb2e4f4389bf94b23fb207
Rahmonali Dev <133217432+rahmonalidev07@users.noreply.github.com>
2023-08-19 13:19:43 +0500

    article-finish-react

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum itaque
+        ducimus vitae soluta commodi, eligendi aliquam aliquid illum, autem
+        recusandae, nesciunt adipisci ab corporis iure. Aliquam quo atque
+        possimus culpa.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        numquam amet doloribus, praesentium rerum exercitationem sunt nobis iure
+        fugit nisi eum explicabo itaque facilis dolores ipsam dolor obcaecati
+        eligendi autem?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
